c5b9ff4fb2c0ee1756cb0eca73c7b2a86f21914e
Moeez Abdul <moeezrajput2000@gmail.com>
2024-03-02 00:04:10 +0000

    Used while loop to indefinitely until a valid input or quitting action is provided

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -1,49 +1,62 @@
 ﻿Console.WriteLine("**- WELCOME TO MO'S BANK -**");
 
-Console.WriteLine("""
-    ** MAIN-MENU **
+while (true)
+{
+    Console.WriteLine("""
+    **- MAIN MENU -**
     Select one of these options:
     1. Sign In
     2. Sign Up
     q. Quit
     """);
 
-Console.Write("Insert option: ");
-String? userInput = Console.ReadLine();
+    Console.Write("Insert option: ");
+    string? userInput = Console.ReadLine()?.ToLower();
 
-if (userInput == "1")
-{
-    Console.WriteLine("""
+    if (userInput == "1")
+    {
+        Console.WriteLine("""
         **- SIGN IN -**
         Please, enter your name and password:
         """);
-    Console.Write("Name: ");
-    String? userName = Console.ReadLine();
-    Console.WriteLine("Password: ");
-    String? userPassword = Console.ReadLine();
-    Console.WriteLine(userName + " " + userPassword);
-} else if (userInput == "2")
-{
-    Console.WriteLine("""
+
+        Console.Write("Name: ");
+        string? userName = Console.ReadLine();
+
+        Console.Write("Password: ");
+        string? userPassword = Console.ReadLine();
+
+        Console.WriteLine($"Username: {userName}");
+        Console.WriteLine($"Password: {userPassword}");
+    }
+    else if (userInput == "2")
+    {
+        Console.WriteLine("""
         **- SIGN UP -**
         Please, enter your name, email and password:
         """);
-    Console.Write("Name: ");
-    String? userName = Console.ReadLine();
-    Console.Write("Email: ");
-    String? userEmail = Console.ReadLine();
-    Console.WriteLine("Password: ");
-    String? userPassword = Console.ReadLine();
-    Console.WriteLine("You have successfully signed up with Mo's Bank!");
-} else if (userInput == "q")
+
+        Console.Write("Name: ");
+        string? userName = Console.ReadLine();
+
+        Console.Write("Email: ");
+        string? userEmail = Console.ReadLine();
+
+        Console.Write("Password: ");
+        string? userPassword = Console.ReadLine();
+
+        Console.WriteLine("You have successfully signed up with Mo's Bank!");
+    }
+    else if (userInput == "1")
     {
-    Console.WriteLine("""
+        Console.WriteLine("""
         **- QUIT -**
         Thank you for using Mo's banking!
         """);
-    return;
-}
-else
-{
-    Console.WriteLine("Oops! Something went wrong. Please, try again!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Oops! Something went wrong. Please, try again!");
+    }
 }
