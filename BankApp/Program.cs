489f546b1083aaca06cfe9f973764c67400b09fe
Moeez Abdul <moeezrajput2000@gmail.com>
2024-03-04 19:28:01 +0000

    Created a a Class for User & a List to store User Data

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -1,62 +1,79 @@
-﻿Console.WriteLine("**- WELCOME TO MO'S BANK -**");
-
-while (true)
+﻿class User
 {
-    Console.WriteLine("""
-    **- MAIN MENU -**
-    Select one of these options:
-    1. Sign In
-    2. Sign Up
-    q. Quit
-    """);
+    public string Name { get; set; }
+    public string email { get; set; }
+    public string password { get; set; }
+}
 
-    Console.Write("Insert option: ");
-    string? userInput = Console.ReadLine()?.ToLower();
-
-    if (userInput == "1")
+class Program
+{
+    static void Main(string[] args)
     {
-        Console.WriteLine("""
-        **- SIGN IN -**
-        Please, enter your name and password:
-        """);
+        List<User> users = new List<User>();
 
-        Console.Write("Name: ");
-        string? userName = Console.ReadLine();
+        Console.WriteLine("**- WELCOME TO MO'S BANK -**");
 
-        Console.Write("Password: ");
-        string? userPassword = Console.ReadLine();
+        while (true)
+        {
+            Console.WriteLine("""
+            **- MAIN MENU -**
+            Select one of these options:
+            1. Sign In
+            2. Sign Up
+            q. Quit
+            """);
 
-        Console.WriteLine($"Username: {userName}");
-        Console.WriteLine($"Password: {userPassword}");
-    }
-    else if (userInput == "2")
-    {
-        Console.WriteLine("""
+            Console.Write("Insert option: ");
+            string? userInput = Console.ReadLine()?.ToLower();
+
+            if (userInput == "1")
+            {
+                Console.WriteLine("""
+                **- SIGN IN -**
+                Please, enter your name and password:
+                """);
+
+                Console.Write("Name: ");
+                string? userName = Console.ReadLine();
+
+                Console.Write("Password: ");
+                string? userPassword = Console.ReadLine();
+
+                Console.WriteLine($"Username: {userName}");
+                Console.WriteLine($"Password: {userPassword}");
+            }
+            else if (userInput == "2")
+            {
+                Console.WriteLine("""
         **- SIGN UP -**
         Please, enter your name, email and password:
         """);
 
-        Console.Write("Name: ");
-        string? userName = Console.ReadLine();
+                Console.Write("Name: ");
+                string? userName = Console.ReadLine();
 
-        Console.Write("Email: ");
-        string? userEmail = Console.ReadLine();
+                Console.Write("Email: ");
+                string? userEmail = Console.ReadLine();
 
-        Console.Write("Password: ");
-        string? userPassword = Console.ReadLine();
+                Console.Write("Password: ");
+                string? userPassword = Console.ReadLine();
 
-        Console.WriteLine("You have successfully signed up with Mo's Bank!");
-    }
-    else if (userInput == "q")
-    {
-        Console.WriteLine("""
+                Console.WriteLine("You have successfully signed up with Mo's Bank!");
+            }
+            else if (userInput == "q")
+            {
+                Console.WriteLine("""
         **- QUIT -**
         Thank you for using Mo's banking!
         """);
-        break;
-    }
-    else
-    {
-        Console.WriteLine("Oops! Something went wrong. Please, try again!");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Oops! Something went wrong. Please, try again!");
+            }
+        }
+
     }
 }
+
